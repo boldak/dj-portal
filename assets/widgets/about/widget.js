79a07ae35741ddd9366bb7d5d2ac735ea12472bb
boldak <boldak.andrey@gmail.com>
2017-11-20 16:52:20 +0200

    create compiled portal for dps testing

--- assets/widgets/about/widget.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-
-angular.module('app.widgets.about', [])
-  .controller('AboutAuthorsController', function ($scope, APIProvider, author) {
-    new APIProvider($scope)
-      .config(() => {
-        $scope.author = author;
-      });
-  });
--- assets/widgets/non-visual-widget/widget.js
+++
@@ -1,3 +0,0 @@
-import angular from 'angular';
-angular.module('app.widgets.non-visual-widget', [])
-  .constant('NonVisualWidgetData', 'This is data from Non-Visual-Widget for demo purposes');
