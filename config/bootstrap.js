119984edad24ac3a686d12fbb3e83258ef957c83
boldak <boldak.andrey@gmail.com>
2017-12-19 13:44:49 +0200

    print env before bootstrap

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -75,5 +75,9 @@ module.exports.bootstrap = function (cb) {
   addDefaultPortalConfig();
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+
+  console.log("Process enviroment:");
+  _.forIn(process.env, (value,key) => {console.log(key+" = "+ value)})//.proxyHost+":"+sails.config.proxyPort);
   cb();
 };
